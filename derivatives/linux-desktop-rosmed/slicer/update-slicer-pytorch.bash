444c70f30ea966bbf411f246eedd1ec396177918
Junichi Tokuda <tokuda@bwh.harvard.edu>
2026-04-19 14:36:16 -0400

    Add LD_LIBRARY_PATH setting to .bashrc. Remove start-slicer-ros2.bash

--- derivatives/linux-desktop-rosmed/slicer/update-slicer-pytorch.bash
+++ derivatives/linux-desktop-rosmed/slicer/update-slicer-pytorch.bash
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+pytorchver=129
+
+while getopts "v:" opt; do
+  case $opt in
+    v) pytorchver=$OPTARG ;;
+    *) echo "Invalid option" ;;
+  esac
+done
+
+echo "Target Pytorch Version: $pytorchver"
+
 
 source /opt/ros/jazzy/setup.sh
 export EXTENSION_DIR=/home/user/slicer/packages
-xvfb-run --auto-servernum /home/user/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer --no-main-window --no-splash --python-script /home/user/slicer/install-pytorch.py; exit 0
+xvfb-run --auto-servernum /home/user/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer --no-main-window --no-splash --python-script /home/user/slicer/update-slicer-pytorch.py $pytorchver; exit 0
--- derivatives/linux-desktop-rosmed/slicer/update-slicer-pytorch.py
+++ derivatives/linux-desktop-rosmed/slicer/update-slicer-pytorch.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import os
 
+pytorchver=129
+
+if len(sys.argv) > 1:
+    pytorchver=int(sys.argv[1])
+
+print("Installing pytorch " + str(pytorchver))
+
+url="https://download.pytorch.org/whl/nightly/cu%d" % pytorchver
+
 slicer.util.pip_install("--upgrade pip")
 slicer.util.pip_uninstall("torch")
 slicer.util.pip_uninstall("torchvision")
-slicer.util.pip_install("--pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/cu129")
+slicer.util.pip_install("--pre torch torchvision --index-url %s" % url)
 
 import torch
 print(torch.cuda.is_available())
-print(torch.version.cuda) # Should show 12.9
+print(torch.version.cuda)
 
 exit()
